fc048fb5d5d614563c0031720ec3201be82f489b
unknown <tiagopimenta.ata@gmail.com>
2024-04-16 22:37:51 -0300

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,85 @@
+const nextDom = document.getElementById("next");
+const prevDom = document.getElementById("prev");
+const carroselDom = document.querySelector(".carrosel");
+const listaItemDom = document.querySelector(".carrosel .lista");
+const thumbnailDom = document.querySelector(".carrosel .thumbnail");
+const navDom = document.querySelector(".nav")
+
+setTimeout(function() {
+    navDom.classList.remove('nav-hidden');
+    navDom.classList.add('nav-visible');
+  }, 2000);
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var walls = document.querySelectorAll('.Wallpapers');
+  
+    function showBoxes() {
+      walls.forEach(function(wall) {
+        var wallPosition = wall.getBoundingClientRect().top;
+        var screenPosition = window.innerHeight / 1;
+  
+        if (wallPosition < screenPosition) {
+            wall.style.opacity = '1';
+            wall.style.transform = 'translateX(0)';
+          } else {
+            wall.style.opacity = '0';
+            if (wall.classList.contains('Wallpapers-left')) {
+              wall.style.transform = 'translateX(-100%)';
+            } else {
+              wall.style.transform = 'translateX(100%)';
+            }
+        }
+      });
+    }
+  
+    showBoxes();
+  
+    window.addEventListener('scroll', showBoxes);
+  });
+
+
+nextDom.onclick = function(){
+    showSlider("next");
+}
+
+prevDom.onclick = function(){
+    showSlider("prev")
+}
+const timeRunnin = 3000;
+const timeAutoNext = 7000;
+let runTimeOut;
+let runAutoRun = setTimeout(() => {
+    nextDom.click();
+}, timeAutoNext)
+
+function showSlider(type){
+    let itemslider = document.querySelectorAll(".carrosel .lista .itens");
+    let itemThumbnail = document.querySelectorAll(".carrosel .thumbnail .item");
+
+    if(type === "next"){
+        listaItemDom.appendChild(itemslider[0]);
+        thumbnailDom.appendChild(itemThumbnail[0]);
+        carroselDom.classList.add("next")
+    } else {
+        let positionLastItem = itemslider.length - 1;
+        listaItemDom.prepend(itemslider[positionLastItem])
+        thumbnailDom.prepend(itemThumbnail[positionLastItem])
+        carroselDom.classList.add("prev")
+    }
+
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+        carroselDom.classList.remove("next")
+        carroselDom.classList.remove("prev")
+    }, timeRunnin);
+
+    clearTimeout(runAutoRun);
+    runAutoRun = setTimeout(() => {
+        nextDom.click();
+    }, timeAutoNext)
+}
+
+
+
+
