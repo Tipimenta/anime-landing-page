7bc988da55789a6e125776c6406a9aa844e0eba9
unknown <tiagopimenta.ata@gmail.com>
2024-04-17 22:18:48 -0300

    atualizada section episodios

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,7 +6,6 @@ const thumbnailDom = document.querySelector(".carrosel .thumbnail");
 const navDom = document.querySelector(".nav")
 
 setTimeout(function() {
-    navDom.classList.remove('nav-hidden');
     navDom.classList.add('nav-visible');
   }, 2000);
 
@@ -24,11 +23,36 @@ document.addEventListener("DOMContentLoaded", function() {
             wall.style.transform = 'translateX(0)';
           } else {
             wall.style.opacity = '0';
-            if (wall.classList.contains('Wallpapers-left')) {
+            if (wall.classList.contains('left')) {
               wall.style.transform = 'translateX(-100%)';
             } else {
               wall.style.transform = 'translateX(100%)';
             }
+        }
+      });
+    }
+  
+    showBoxes();
+  
+    window.addEventListener('scroll', showBoxes);
+  });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var eps = document.querySelectorAll('.ep');
+  
+    function showBoxes() {
+      eps.forEach(function(ep) {
+        var epPosition = ep.getBoundingClientRect().top;
+        var screenPosition = window.innerHeight / 1.5;
+  
+        if (epPosition < screenPosition) {
+            ep.style.opacity = '1';
+            ep.style.transform = 'translateY(0)';
+          } else {
+            ep.style.opacity = '0';
+            if (ep.classList.contains('nimated.moveUp')) {
+              ep.style.transform = 'translateY(-100%)';
+            } 
         }
       });
     }
